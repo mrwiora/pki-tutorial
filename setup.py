ad797a48c186d03fee32c2e9738e9345016fefdf
Stefan H. Holek <stefan@epy.co.at>
2012-12-03 19:13:18 +0100

    Add setup.py to create the build environment.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+version = '0.9'
+
+setup(name='pki-tutorial',
+      version=version,
+      description='OpenSSL PKI Tutorial',
+      author='Stefan H. Holek',
+      author_email='stefan@epy.co.at',
+      url='http://pki-tutorial.readthedocs.org/',
+      license='CC',
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'sphinx',
+          'pygments-openssl',
+      ],
+)
