d7cc1a669cc7a065d63c1dd43a5eae46fb511ae7
Stefan H. Holek <stefan@epy.co.at>
2014-03-06 21:06:39 +0100

    Add Twitter handle to sig.

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'OpenSSL PKI Tutorial'
-copyright = u'2012-2014, Stefan H. Holek'
+copyright = u'2012-2014, Stefan H. Holek, @stefanholek'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
